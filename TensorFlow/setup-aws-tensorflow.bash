dba6e3ef738286052ceffbe8744f36b3a5841a27
mGalarnyk <mgalarny@gmail.com>
2016-09-07 20:15:01 -0500

    tensorflow install

---
+++ TensorFlow/setup-aws-tensorflow.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# stop on error
+set -e
+############################################
+# install into /mnt/bin
+sudo mkdir -p /mnt/bin
+sudo chown ubuntu:ubuntu /mnt/bin
+
+# install the required packages
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
+
+# install cuda
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo apt-get update
+sudo apt-get install -y cuda
+
+# get cudnn
+CUDNN_FILE=cudnn-7.0-linux-x64-v4.0-prod.tgz
+wget http://developer.download.nvidia.com/compute/redist/cudnn/v4/${CUDNN_FILE}
+tar xvzf ${CUDNN_FILE}
+rm ${CUDNN_FILE}
+sudo cp cuda/include/cudnn.h /usr/local/cuda/include # move library files to /usr/local/cuda
+sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
+sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
+rm -rf cuda
+
+# set the appropriate library path
+echo 'export CUDA_HOME=/usr/local/cuda
+export CUDA_ROOT=/usr/local/cuda
+export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
+' >> ~/.bashrc
+
+# install anaconda
+wget https://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh
+bash Anaconda2-4.1.1-Linux-x86_64.sh -b -p /mnt/bin/anaconda2
+rm Anaconda2-4.1.1-Linux-x86_64.sh
+echo 'export PATH="/mnt/bin/anaconda2/bin:$PATH"' >> ~/.bashrc
+
+# install tensorflow
+export TF_BINARY_URL='https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl'
+
+/mnt/bin/anaconda2/bin/pip install $TF_BINARY_URL
+
+# install monitoring programs
+sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
+sudo chmod +x /usr/local/bin/gpustat
+sudo nvidia-smi daemon
+sudo apt-get -y install htop
+
+# reload .bashrc
+exec bash
+
