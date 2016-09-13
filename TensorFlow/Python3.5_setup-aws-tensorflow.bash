937f94ac485826b9b17d518aff276e862f4ffd20
mGalarnyk <mgalarny@gmail.com>
2016-09-13 01:00:02 -0500

    bash

--- TensorFlow/Python3.5_setup-aws-tensorflow.bash
+++ TensorFlow/Python3.5_setup-aws-tensorflow.bash
@@ -36,15 +36,15 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
 ' >> ~/.bashrc
 
 # install anaconda
-wget https://repo.continuum.io/archive/Anaconda2-4.1.1-Linux-x86_64.sh
-bash Anaconda2-4.1.1-Linux-x86_64.sh -b -p /mnt/bin/anaconda2
-rm Anaconda2-4.1.1-Linux-x86_64.sh
-echo 'export PATH="/mnt/bin/anaconda2/bin:$PATH"' >> ~/.bashrc
+wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p /mnt/bin/anaconda3
+rm Anaconda3-4.0.0-Linux-x86_64.sh
+echo 'export PATH="/mnt/bin/anaconda3/bin:$PATH"' >> ~/.bashrc
 
 # install tensorflow
 export TF_BINARY_URL='https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl'
 
-/mnt/bin/anaconda2/bin/pip install $TF_BINARY_URL
+/mnt/bin/anaconda3/bin/pip install $TF_BINARY_URL
 
 # install monitoring programs
 sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
@@ -54,4 +54,10 @@ sudo apt-get -y install htop
 
 # reload .bashrc
 exec bash
-
+############################################
+# run the test
+# byobu				# start byobu + press Ctrl + F2 
+# htop				# run in window 1, press Shift + F2
+# watch --color -n1.0 gpustat -cp	# run in window 2, press Shift + <left>
+# wget https://raw.githubusercontent.com/tensorflow/tensorflow/master/tensorflow/models/image/mnist/convolutional.py
+# python convolutional.py		# run in window 3
